dec56a30459ad0e684298a4c4b5b9569adccb4a1
Arpit-Rajawat <arpitrajawat18@gmail.com>
2022-03-31 16:14:32 +0530

    Initial Commit

---
+++ assets/js/footer.js
@@ -0,0 +1,30 @@
+let footer_div = `<footer class="bg-theme-dark">
+        <div class="container">
+          <div class="row align-items-center gutter-1 gutter-lg-6 text-muted">
+            <div class="col-md-6 col-lg-6 order-lg-2">
+              <ul class="nav nav-minimal nav-minimal-sm">
+                <li class="nav-item">
+                    <a class="nav-link footer-link nav-link-hover" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link footer-link nav-link-hover" href="#"><i class="fab fa-linkedin-in"></i> Linkedin</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link footer-link nav-link-hover" href="#"><i class="fab fa-dribbble"></i> Dribbble</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link footer-link nav-link-hover" href="#"><i class="fab fa-behance"></i> Behance</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-12 col-lg-3 order-lg-1">
+                <a class="copy_text"  data-toggle="tooltip" title="Copy to Clipboard" href="arpitrajawat18@gmail.com">arpitrajawat18@gmail.com</a>
+            </div>
+            <div class="col-md-12 col-lg-3 text-md-right order-lg-3">
+              <p class=""><i class="fas fa-paint-brush"></i> Crafted by me</p>
+            </div>
+          </div>
+        </div>
+      </footer>`;
+
+$("#footerDiv").html(footer_div);
---
+++ assets/js/header.js
@@ -0,0 +1,40 @@
+let homeLink = "index.html";
+let myWorkLink = "index.html#myWork";
+let aboutmeLink = "aboutme.html"
+
+let header_div = `<header class="header header-sticky header-light">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a href="${homeLink}" class="navbar-brand mr-8 d-flex align-items-center">
+              <img src="assets/images/ARIcon.svg" alt="">
+              <h4 class="ml-1 display-5">Arpit Rajawat</h4>
+          </a>
+
+          <button class="navbar-toggler burger" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item dropdown dropdown-hover">
+                <a class="nav-link header-link nav-link-hover" href="${aboutmeLink}" id="" role="button"  aria-haspopup="true" aria-expanded="false">
+                  About me
+                </a>
+              </li>
+              <li class="nav-item dropdown dropdown-hover">
+                <a class="nav-link header-link nav-link-hover" href="${myWorkLink}" id="" role="button" aria-haspopup="true" aria-expanded="false">
+                  Work
+                </a>
+              </li>
+              <li class="nav-item dropdown dropdown-hover">
+                <a class="nav-link header-link nav-link-hover" href="#" id="" role="button" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-arrow-down"></i> Download Resume
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>`;
+
+$("#headerDiv").html(header_div);
