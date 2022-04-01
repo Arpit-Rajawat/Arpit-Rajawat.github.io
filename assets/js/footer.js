8fe374e8b9abc1f51df541f60fcea27a58702606
Arpit-Rajawat <arpitrajawat18@gmail.com>
2022-04-02 01:34:26 +0530

    commiit

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,7 +1,7 @@
 let footer_div = `<footer class="bg-theme-dark">
         <div class="container">
           <div class="row align-items-center gutter-1 gutter-lg-6 text-muted">
-            <div class="col-md-6 col-lg-6 order-lg-2">
+            <div class="col-12 col-md-12 col-lg-6 order-lg-2">
               <ul class="nav nav-minimal nav-minimal-sm">
                 <li class="nav-item">
                     <a class="nav-link footer-link nav-link-hover" href="https://www.instagram.com/arpit__rajawat/" target="_blank"><i class="fab fa-instagram"></i> Instagram</a>
@@ -17,10 +17,10 @@ let footer_div = `<footer class="bg-theme-dark">
                 </li>
               </ul>
             </div>
-            <div class="col-md-12 col-lg-3 order-lg-1">
+            <div class="col-md-6 col-lg-3 order-lg-1">
                 <a class="copy_text"  data-toggle="tooltip" title="Copy to Clipboard" href="arpitrajawat18@gmail.com">arpitrajawat18@gmail.com</a>
             </div>
-            <div class="col-md-12 col-lg-3 text-md-right order-lg-3">
+            <div class="col-md-6 col-lg-3 text-md-right order-lg-3">
               <p class=""><i class="fas fa-paint-brush"></i> Crafted by me</p>
             </div>
           </div>
